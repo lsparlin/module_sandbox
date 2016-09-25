9742a9bb74cc4e7f05fecaf129ed517e879d8d79
Lewis Sparlin <lewis@edatasource.com>
2016-09-25 16:43:08 -0500

    Added compilation step for service to shell script

--- compile_and_run_java.sh
+++ compile_and_run_java.sh
@@ -1,6 +1,9 @@
 javac -d mods/dep \
 	$(find dep_mod -name "*.java")
 
+javac -d mods/service \
+  $(find service_mod  -name "*.java")
+
 javac --module-path mods \
 	-d mods/hello \
 	$(find hello_mod  -name "*.java")
